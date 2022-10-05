cd0eb73aba297787dfa5a4ca6491b9842cd5864f
mnpostree <110006066+edelweiscoin@users.noreply.github.com>
2022-10-05 10:04:00 +0800

    Protocol Version 70201

--- src/version.h
+++ src/version.h
@@ -12,7 +12,7 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 70200;
+static const int PROTOCOL_VERSION = 70201;
 
 //! disconnect from peers older than this proto version
 static const int MIN_PEER_PROTO_VERSION = PROTOCOL_VERSION;
